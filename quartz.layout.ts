6b4185fc29d9066e9cf497ea0d3fbbfabe14c02b
Daniel <dshu.mcs@gmail.com>
2024-02-06 18:52:10 -0800

    Quartz sync: Feb 6, 2024, 6:52 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -47,3 +47,32 @@ export const defaultListPageLayout: PageLayout = {
   ],
   right: [],
 }
+
+Component.Graph({
+  localGraph: {
+    drag: true, // whether to allow panning the view around
+    zoom: true, // whether to allow zooming in and out
+    depth: 1, // how many hops of notes to display
+    scale: 1.1, // default view scale
+    repelForce: 0.5, // how much nodes should repel each other
+    centerForce: 0.3, // how much force to use when trying to center the nodes
+    linkDistance: 30, // how long should the links be by default?
+    fontSize: 0.6, // what size should the node labels be?
+    opacityScale: 1, // how quickly do we fade out the labels when zooming out?
+    removeTags: [], // what tags to remove from the graph
+    showTags: true, // whether to show tags in the graph
+  },
+  globalGraph: {
+    drag: true,
+    zoom: true,
+    depth: -1,
+    scale: 0.9,
+    repelForce: 10,
+    centerForce: 0.52,
+    linkDistance: 250,
+    fontSize: 0.3,
+    opacityScale: 1,
+    removeTags: [], // what tags to remove from the graph
+    showTags: true, // whether to show tags in the graph
+  },
+})
