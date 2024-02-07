12bab63ec376ee04a0c4024714afa013e9fdb18f
Daniel <dshu.mcs@gmail.com>
2024-02-06 20:17:25 -0800

    Quartz sync: Feb 6, 2024, 8:17 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -49,7 +49,7 @@ export const defaultContentPageLayout: PageLayout = {
         zoom: true,
         depth: -1,
         scale: 0.9,
-        repelForce: 2,
+        repelForce: 0.1,
         centerForce: 0.3,
         linkDistance: 30,
         fontSize: 0.6,
