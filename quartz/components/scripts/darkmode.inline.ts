e1de60b16297bc992f3c6eb407987c0c5e6d997d
Daniel <dshu.mcs@gmail.com>
2024-03-30 10:41:20 -0700

    Quartz sync: Mar 30, 2024, 10:41 AM

--- quartz/components/scripts/darkmode.inline.ts
+++ quartz/components/scripts/darkmode.inline.ts
@@ -1,5 +1,5 @@
 const userPref = window.matchMedia("(prefers-color-scheme: light)").matches ? "light" : "dark"
-const currentTheme = localStorage.getItem("theme") ?? userPref
+const currentTheme = "dark"
 document.documentElement.setAttribute("saved-theme", currentTheme)
 
 const emitThemeChangeEvent = (theme: "light" | "dark") => {
