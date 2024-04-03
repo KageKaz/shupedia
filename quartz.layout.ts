d3da3a782d11ed55af180b2f64b68fea40735221
Daniel <dshu.mcs@gmail.com>
2024-04-02 18:30:20 -0700

    Quartz sync: Apr 2, 2024, 6:30 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -34,7 +34,7 @@ export const defaultContentPageLayout: PageLayout = {
         drag: true, // whether to allow panning the view around
         zoom: true, // whether to allow zooming in and out
         depth: 1, // how many hops of notes to display
-        scale: 1.1, // default view scale
+        scale: 1.5, // default view scale
         repelForce: 0.5, // how much nodes should repel each other
         centerForce: 0.3, // how much force to use when trying to center the nodes
         linkDistance: 30, // how long should the links be by default?
