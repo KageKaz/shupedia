4998b0558b4eb8e48399e265165ee319209c6a84
Daniel <dshu.mcs@gmail.com>
2024-04-04 21:46:08 -0700

    Quartz sync: Apr 4, 2024, 9:46 PM

--- quartz/plugins/transformers/latex.ts
+++ quartz/plugins/transformers/latex.ts
@@ -8,7 +8,7 @@ interface Options {
 }
 
 export const Latex: QuartzTransformerPlugin<Options> = (opts?: Options) => {
-  const engine = opts?.renderEngine ?? "mathjax"
+  const engine = opts?.renderEngine ?? "katex"
   return {
     name: "Latex",
     markdownPlugins() {
