c3b020d42fe6b5381211f52a3bf095e61477185b
Daniel <dshu.mcs@gmail.com>
2024-06-05 12:45:08 -0700

    Quartz sync: Jun 5, 2024, 12:45 PM

--- quartz/plugins/transformers/lastmod.ts
+++ quartz/plugins/transformers/lastmod.ts
@@ -5,11 +5,11 @@ import { QuartzTransformerPlugin } from "../types"
 import chalk from "chalk"
 
 export interface Options {
-  priority: ("git" | "frontmatter" | "filesystem")[]
+  priority: ("frontmatter" | "git" | "filesystem")[]
 }
 
 const defaultOptions: Options = {
-  priority: ["git", "frontmatter", "filesystem"],
+  priority: ["frontmatter", "git", "filesystem"],
 }
 
 function coerceDate(fp: string, d: any): Date {
