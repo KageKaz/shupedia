235334b92f019f62624a4e0fe163f453995d5d6e
Daniel <dshu.mcs@gmail.com>
2024-06-05 18:00:23 -0700

    Quartz sync: Jun 5, 2024, 6:00 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -61,7 +61,7 @@ const config: QuartzConfig = {
       Plugin.CreatedModifiedDate({
         // you can add 'git' here for last modified from Git
         // if you do rely on git for dates, ensure defaultDateType is 'modified'
-        priority: ["frontmatter"],
+        priority: ["git"],
       }),
       Plugin.Latex({ renderEngine: "katex" }),
       Plugin.SyntaxHighlighting(),
